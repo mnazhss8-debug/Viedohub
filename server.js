48df5db2f28b37feeaa5d3bf96dbce458d2b189e
mnazhss8-debug <mnazhss8@gmail.com>
2026-01-18 15:53:16 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -34,18 +34,27 @@ const upload = multer({ storage: cloudStorage });
 app.use(express.static('.'));
 
 // 4. Endpointy
+// ... (początek bez zmian)
+
+// Endpoint listy filmów (teraz z sortowaniem od najnowszych)
 app.get('/list-videos', (req, res) => {
     db.find({}).sort({ timestamp: -1 }).exec((err, docs) => res.send(docs));
 });
 
+// Endpoint wysyłania (dodajemy views: 0)
 app.post('/upload', upload.single('video'), (req, res) => {
     if (!req.file) return res.status(400).send('Brak pliku');
     
-    // Ważna zmiana: Cloudinary zwraca link w req.file.path
+    // Cloudinary automatycznie generuje miniaturkę JPG dla każdego wideo pod tym samym linkiem, zmieniając tylko rozszerzenie
+    const thumbnailPath = req.file.path.replace(/\.[^/.]+$/, ".jpg");
+
     const videoData = {
         title: req.file.originalname || "Bez tytułu",
-        path: req.file.path, // To będzie teraz link https://res.cloudinary.com/...
-        timestamp: Date.now()
+        path: req.file.path,
+        thumbnail: thumbnailPath,
+        views: 0, // Nowa funkcja: licznik
+        timestamp: Date.now(),
+        public_id: req.file.filename // Potrzebne do usuwania z Cloudinary
     };
     
     db.insert(videoData, (err, newDoc) => {
@@ -53,5 +62,27 @@ app.post('/upload', upload.single('video'), (req, res) => {
         res.send(newDoc);
     });
 });
+
+// Nowy endpoint: Zliczanie wyświetleń
+app.post('/view/:id', (req, res) => {
+    db.update({ _id: req.params.id }, { $inc: { views: 1 } }, {}, () => {
+        res.sendStatus(200);
+    });
+});
+
+// Nowy endpoint: Usuwanie filmu
+app.delete('/video/:id', (req, res) => {
+    // Najpierw pobieramy dane o filmie, żeby znać public_id w Cloudinary
+    db.findOne({ _id: req.params.id }, (err, video) => {
+        if (video) {
+            // Usuwamy z Cloudinary
+            cloudinary.uploader.destroy(video.public_id, { resource_type: 'video' });
+            // Usuwamy z bazy danych
+            db.remove({ _id: req.params.id }, {}, () => res.sendStatus(200));
+        }
+    });
+});
+
+// ... (reszta kodu)
 
 app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
